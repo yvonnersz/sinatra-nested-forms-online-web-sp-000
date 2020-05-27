6040943d5b8610617970470253ae0c2fdb98b940
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-27 23:04:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,26 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      "Welcome to the Nested Forms Lab!"
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :"pirates/show"
+
+    end
+
   end
 end
